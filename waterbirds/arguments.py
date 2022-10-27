234d102bed36d34f9e9f9e655538e72fe1db359a
Akshaj Kumar Veldanda <44175190+akshajkumarv@users.noreply.github.com>
2022-10-27 11:51:57 -0400

    updated_args

--- waterbirds/arguments.py
+++ waterbirds/arguments.py
@@ -1,14 +1,14 @@
 import argparse
 
 parser = argparse.ArgumentParser(description='Params')
-parser.add_argument('--lr', type=float, default=1e-04)
+parser.add_argument('--lr', type=float, default=1e-05)
 parser.add_argument('--run', type =int, default=1)
 parser.add_argument('--batch_size', type=int, default=64)
 parser.add_argument('--epochs', type=int, default = 300)
 parser.add_argument('--stage_1_epoch', type=int, default = 50)
 parser.add_argument('--upsample', type=int, default = 20)
 parser.add_argument('--momentum', type=float, default=0.9) 
-parser.add_argument('--weight_decay', type=float, default=1e-01) 
+parser.add_argument('--weight_decay', type=float, default=1.0) 
 
 
 args = parser.parse_args()
